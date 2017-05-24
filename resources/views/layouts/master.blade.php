8f4feb36aebc42bd21d9aa6f86b497282bb1b45d
alexandrehamed <alexandrehamed@hotmail.fr>
2017-05-24 14:57:58 +0200

    remove container layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -117,9 +117,7 @@
     </div>
 </nav>
 
-<div class="container">
     @yield('content')
-</div>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
