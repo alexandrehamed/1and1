60291c85d802407b9527419e803e74d3c689ad42
Cakebool <cakeboool@gmail.com>
2017-05-25 19:22:33 +0200

    design page profil

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,30 +4,40 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
-                <img src="/images/{{ $user->avatar }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
-                <h2>Profile de {{ $user->name }}</h2>
-                <form enctype="multipart/form-data" action="{{route('profile.avatar')}}" method="POST">
-                    <label>Update Profile Image</label>
-                    <input type="file" name="avatar">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                    <input type="submit" class="pull-right btn btn-sm btn-primary">
-                </form>
-                <h3>Inscrit le : {{ $user->created_at }}</h3>
-                <form method="POST" action="{{route('profile.update', $user->id)}}">
-                    {{csrf_field()}}
-                    <input type="hidden" name="_method" value="put">
-                    <h3>Nom :</h3>
-                    <input required type="text" value="{{$user->name}}" name="name" class="form-control">
-                    <br>
-                    <h3>email :</h3>
-                    <input required type="text" value="{{$user->email}}" name="email" class="form-control">
-                    <br>
-                    <h3>telephone :</h3>
-                    <input required type="text" value="{{$user->number}}" name="number" class="form-control">
-                    <br>
-                    <br>
-                    <input type="submit" value="Sauvegarder">
-                </form>
+                <div class="fiche">
+                    <div class="head-fiche">
+                        <img src="/images/avatar/{{ $user->avatar }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
+                        <h2>Profile de {{ $user->name }}</h2>
+                        <form enctype="multipart/form-data" action="{{route('profile.avatar')}}" method="POST">
+                            <label>Changer l'avatar</label>
+                            <input type="file" name="avatar">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <br>
+                            <input type="submit" class="btn btn-sm ">
+                        </form>
+                        <h3>Inscrit le : {{ $user->created_at }}</h3>
+                    </div>
+                    <div class="content-fiche">
+                        <form method="POST" action="{{route('profile.update', $user->id)}}">
+                            {{csrf_field()}}
+                            <input type="hidden" name="_method" value="put">
+                            <h3>Nom :</h3>
+                            <input required type="text" value="{{$user->name}}" name="name" class="form-control">
+                            <br>
+                            <h3>email :</h3>
+                            <input required type="text" value="{{$user->email}}" name="email" class="form-control">
+                            <br>
+                            <h3>telephone :</h3>
+                            <input required type="text" value="{{$user->number}}" name="number" class="form-control">
+                            <br>
+                            <br>
+                            <input type="submit" value="Sauvegarder" class="btn btn-form">
+                        </form>
+                    </div>
+
+
+                </div>
+
             </div>
         </div>
     </div>
